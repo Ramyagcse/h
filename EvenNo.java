15db4584a246d470fb4650fdf9314875fbde29d5
Ramyagcse <ramyashines31@gmail.com>
2016-08-08 11:53:04 +0530

    Add files via upload

---
+++ EvenNo.java
@@ -0,0 +1,16 @@
+public class EvenNo
+{
+public static void main(String[]args)
+{
+Scanner s=new Scanner(System.in);
+System.out.println("enter the number");
+int n=s.next.Int();
+for(int i=0;i<=10;i++)
+{
+if(n%2==0)
+{
+System.out.println("the number is even");
+}
+}
+}
+}
